812a78af9c5976a7f6675121bca4b0a219e53780
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-05-24 19:58:21 -0500

    Generated blog scaffold and db migrate

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -17,7 +17,7 @@ class ContactsController < ApplicationController
       redirect_to new_contact_path
     end
   end
-  
+
   private
   def contact_params
     params.require(:contact).permit(:name, :email, :comments)
