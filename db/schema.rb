c5e19ac48f7003317d57778f88ee13c2875d6a8c
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-05-29 12:05:27 -0500

    fixed contact form page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525003354) do
+ActiveRecord::Schema.define(version: 20170529164526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
