9456da18f11fb591311696d9e7ff4a12b0a9a0c6
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-05-29 20:18:09 -0500

    revert back on enviornment

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.smtp_settings = {
-  :port           => 587,
-  :address        => 'smtp.mailgun.org'
-  :user_name      => ENV['MAILGUN_USERNAME'],
-  :password       => ENV['MAILGUN_PASSWORD'],
-  :domain         => 'shrouded-fortress-66409.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'shrouded-fortress-66409.herokuapp.com',
   :authentication => :plain,
 }
+
 ActionMailer::Base.delivery_method = :smtp
