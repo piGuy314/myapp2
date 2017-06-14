5f012b037ab4022298e1184a8934292e9a5f2df9
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-06-13 20:09:08 -0500

    Added comments to mock sign in form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          extend Devise::Models
+
   belongs_to :plan
 end
