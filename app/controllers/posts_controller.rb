9c5a1749bf66de8b78a7ed60b33edb82cbe24781
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-06-13 22:12:50 -0500

    updated post section with decending post order and post date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,13 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+
   end
 
   # GET /posts/new
