3379a4e5220e703d01e0f8d8153104ec788d0ecc
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-07-06 21:38:21 -0500

    Added comment functionality to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   get 'about', to: 'pages#about'
   resources :contacts
   get 'contact-us', to: 'contacts#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529164526) do
+ActiveRecord::Schema.define(version: 20170707014147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -47,4 +56,5 @@ ActiveRecord::Schema.define(version: 20170529164526) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
