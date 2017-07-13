ddd354ece81933522ad26b2ee4db9950ad657867
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-07-13 18:22:32 -0500

    changed ruby version

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+config.assets.initialize_on_precompile = false
+
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
